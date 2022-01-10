45e7aeacc24922ee3c6514ecfe5d8e3e3ac899e8
申诚 付 <fsc407074468@hotmail.com>
2022-01-10 11:51:04 -0500

    session 1-1

---
+++ FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FavoriteColorAndNumber
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string favColor = null;
+
+            int nFavNumber = 0;
+            short nShortFavNumber = 0;
+
+            favColor.ToUpper();
+
+            Console.ReadLine();
+        }
+    }
+}
