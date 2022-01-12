7cf24a177dedcc3bb589ee5530f7631e089a93fb
申诚 付 <fsc407074468@hotmail.com>
2022-01-12 12:05:30 -0500

    PE1

---
+++ Fu_HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        //Method: main
+        //Purpose: to print characters in the consoler
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 7)
+            {
+                i++;
+            }
+            Console.WriteLine($"There are {i} days in a week!");
+        }
+    }
+}
