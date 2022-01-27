ccb985a3b86cb1f2d4052569f65f9d7b8711172f
申诚 付 <fsc407074468@hotmail.com>
2022-01-26 21:30:48 -0500

    PE6

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    // Class Program
+    // Author: Shencheng Fu 
+    // Purpose: Number guessing game
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: generate a random integer between 0 and 100
+        // let the use have 8 guesses what the is
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            // generate a random number between 0 inclusive and 101 exclusive
+            int randomNumber = rand.Next(0, 101);
+            int i = 0;
+            int userGuess = 0;
+            int turn = 1;
+
+            Console.WriteLine(randomNumber);
+            Console.WriteLine("Please take guesses between 0 and 100.");
+            // print the number on the console window
+
+
+            for (i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Turn #{0}: Enter your guess: ",turn);
+                if(Int32.TryParse(Console.ReadLine(),out userGuess) && (userGuess >= 0 && userGuess <= 100))
+                {
+                    // tell if the guess is too high or too low
+                    if (userGuess > randomNumber)
+                    {
+                        Console.WriteLine("Too high");
+                    }
+                    else if(userGuess < randomNumber)
+                    {
+                        Console.WriteLine("Too low");
+                    }
+                    else
+                    {
+                        // exit the loop if the guess is right
+                        i = 8;
+                    }
+                }
+                // caution if the input is invalid
+                else
+                {
+                    Console.WriteLine("Invalid guess – try again");
+                    i--;
+                    turn--;
+                }
+                turn++;
+            }
+
+            if(turn < 8)
+            {
+                // print the turns user used
+                Console.WriteLine("Correct! You won in {0} turns.",turn-1);
+            }
+            else
+            {
+                // print the number if user failed
+                Console.WriteLine("You ran out of turns. The number was {0}.", randomNumber);
+            }
+        }
+    }
+}
