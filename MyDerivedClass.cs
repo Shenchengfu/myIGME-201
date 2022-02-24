2624b2ffe8b5b7b1ec6ba9740a033f4378733aef
申诚 付 <fsc407074468@hotmail.com>
2022-02-24 14:42:29 -0500

    PE12

---
+++ MyDerivedClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class MyDerivedClass : MyClass
+{
+    public MyDerivedClass(string s) : base(s) {
+    }
+
+    public override string GetString()
+    {
+        return base.GetString() + " (output from the derived class)";
+    }
+
+    static void Main() {
+        MyClass mc = new MyDerivedClass("Hello!");
+        Console.WriteLine(mc.GetString());
+    }
+}
